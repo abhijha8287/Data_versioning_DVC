263ed6d2926c44e5f28a0bd7d7b8351fee4a6c44
Abhishek <abhijha8287@gmail.com>
2025-12-25 01:03:18 +0530

    third version of data saved

--- mycode.py
+++ mycode.py
@@ -10,6 +10,8 @@ df=pd.DataFrame(data)
 new_row = {"Name": "David", "Age": 28, "City": "San Francisco"}
 df.loc[len(df)] = new_row
 
+new_row2 = {"Name": "Eva", "Age": 22, "City": "Boston"}
+df.loc[len(df)] = new_row2
 
 data_dir = "data"
 os.makedirs(data_dir, exist_ok=True)
